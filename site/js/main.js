d7a95a9f09edb364dd872dc40463070feb7592b6
Dylanfreirinho <46023502+Dylanfreirinho@users.noreply.github.com>
2020-10-26 21:33:26 +0100

    Add files via upload

---
+++ site/js/main.js
@@ -0,0 +1,15 @@
+//smooth scrollen
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+          scrollTop: $(hash).offset().top - 80
+      },
+      800
+    );
+  }
+});
